71297b4fb2de967f48e83954096fb39024819dfd
Kevin ACHAMANA <achamanakevin.dev@gmail.com>
2024-01-02 12:15:18 +0100

    instalation boostrap

--- src/DataFixtures/CategoryFixtures.php
+++ src/DataFixtures/CategoryFixtures.php
@@ -9,9 +9,18 @@ class CategoryFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        $category = new Category();
-        // $manager->persist($product);
+        $red = new Category();
+        $red->setName('Rouge');
+        $manager->persist($red);
 
+        $white = new Category();
+        $white->setName('Blanc');
+        $manager->persist($white);
+        
+        $pink = new Category();
+        $pink->setName('Rosé');
+        $manager->persist($pink);
+        
         $manager->flush(); 
     }
 }
